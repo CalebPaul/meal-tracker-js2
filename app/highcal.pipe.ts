2e1d09bf46b673ce8c4a642513b62e22c407c498
Caleb Paul <calebpaul21@gmail.com>
2016-10-21 11:44:45 -0700

    add highcal pipe

---
+++ app/highcal.pipe.ts
@@ -0,0 +1,19 @@
+import {Pipe, PipeTransform} from '@angular/core';
+import {Meal} from './meal.model';
+
+@Pipe({
+  name: "highcal",
+  pure: false
+})
+
+export class HighCalPipe implements PipeTransform {
+  transform(input: Meal[]) {
+    var output: Meal[] = [];
+    for (var i = 0; i < input.length; i++) {
+      if (input[i].calories >= 500 ) {
+        output.push(input[i]);
+      }
+    }
+    return output;
+  }
+}

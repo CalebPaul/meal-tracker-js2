ee407ae5cb36c6c43ba86bd760568c7074573979
Caleb Paul <calebpaul21@gmail.com>
2016-10-21 11:59:16 -0700

    add branching logic to calorie pipe for high and low values

--- app/calorie.pipe.ts
+++ app/calorie.pipe.ts
@@ -7,13 +7,26 @@ import {Meal} from './meal.model';
 })
 
 export class CaloriePipe implements PipeTransform {
-  transform(input: Meal[]) {
+  transform(input: Meal[], desiredCal) {
     var output: Meal[] = [];
-    for (var i = 0; i < input.length; i++) {
-      if (input[i].calories >= 500 ) {
-        output.push(input[i]);
+    if (desiredCal === "high") {
+      for (var i = 0; i < input.length; i++) {
+        if (input[i].calories >= 500 ) {
+          output.push(input[i]);
+        }
       }
+      return output;
+    } else if (desiredCal === "low") {
+      for (var i = 0; i < input.length; i++) {
+        if (input[i].calories < 500 ) {
+          output.push(input[i]);
+        }
+      }
+      return output;
+    } else {
+      return input;
     }
-    return output;
+
+
   }
 }

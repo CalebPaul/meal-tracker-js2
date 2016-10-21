eba7a63da4e2106d18f4f8f01f16009c0371624c
Caleb Paul <calebpaul21@gmail.com>
2016-10-21 10:46:42 -0700

    add newmeal component ts file

---
+++ app/new-meal.component.ts
@@ -0,0 +1,37 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Meal } from './meal.model';
+
+@Component({
+  selector: 'new-meal',
+  template: `
+  <div>
+    <h3>Add New Meal</h3>
+    <div>
+      <label>Meal Name</label>
+      <input #newName>
+    </div>
+    <div>
+      <label>Ingredients:</label>
+      <input #newDetails>
+    </div>
+    <div>
+      <label>Calories:</label>
+      <input #newCalories>
+      <button (click)="
+        addClicked(newName.value, newDetails.value, newCalories.value);
+        newName.value='';
+        newDetails.value='';
+        newCalories.value='';
+      ">Add</button>
+    </div>
+  </div>
+  `
+})
+
+export class NewMealComponent {
+  @Output() newMealSender = new EventEmitter();
+  addClicked(name: string, details:string, calories: number) {
+    var newMealToAdd: Meal = new Meal (name, details, calories);
+    this.newMealSender.emit(newMealToAdd);
+  }
+}
